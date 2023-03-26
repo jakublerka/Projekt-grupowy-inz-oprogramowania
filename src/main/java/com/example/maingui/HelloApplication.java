3de31acf7a87d5f2cbd5a5f3dfaed389634fe7f5
jakublerka <jakublerka01@gmail.com>
2023-03-26 21:03:40 +0200

    More features

--- src/main/java/com/example/maingui/HelloApplication.java
+++ src/main/java/com/example/maingui/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.maingui;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -32,41 +34,39 @@ public class HelloApplication extends Application {
         grid.setAlignment(Pos.TOP_LEFT);
         grid.setHgap(10);
         grid.setVgap(10);
-        grid.setPadding(new Insets(25,25,25,25));
+        grid.setPadding(new Insets(25, 25, 25, 25));
 
         Text scenetitle = new Text("SQL generator");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(scenetitle, 0,0,2,1);
+        grid.add(scenetitle, 0, 0, 2, 1);
 
         Label raport = new Label("Raport:");
-        grid.add(raport, 0,1);
+        grid.add(raport, 0, 1);
 
         ObservableList<String> raporty =
                 FXCollections.observableArrayList(
                         "Raport 1",
                         "Raport 2",
-                        "Raport 3",
-                        "Raport 4"
+                        "Raport 3"
                 );
         final ComboBox comboBox = new ComboBox(raporty);
-        grid.add(comboBox, 0,2);
-
+        grid.add(comboBox, 0, 2);
 
 
         Label data = new Label("Data:");
         grid.add(data, 0, 4);
         DatePicker datePicker = new DatePicker();
-        grid.add(datePicker, 0,5);
+        grid.add(datePicker, 0, 5);
 
         Label ID = new Label("ID:");
         grid.add(ID, 8, 4);
         TextField idInput = new TextField("");
-        grid.add(idInput, 8,5);
+        grid.add(idInput, 8, 5);
 
         Label kraj = new Label("Kraj:");
         grid.add(kraj, 16, 4);
         TextField krajInput = new TextField("");
-        grid.add(krajInput, 16,5);
+        grid.add(krajInput, 16, 5);
 
         Label miasto = new Label("Miasto:");
         grid.add(miasto, 24, 4);
@@ -81,22 +81,38 @@ public class HelloApplication extends Application {
                         "Web"
                 );
         ComboBox appWebChoice = new ComboBox(appWebLista);
-        grid.add(appWebChoice, 32,5);
+        grid.add(appWebChoice, 32, 5);
 
-        /*
-        TextArea sqlOutput = new TextArea("test");
-        sqlOutput.setEditable(false);
-        grid.add(sqlOutput, 0, 15, 60, 40);
-         */
+        Button generateButton = new Button("Generate");
+        grid.add(generateButton, 40, 5);
+        EventHandler<ActionEvent> buttonHandler = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                idInput.getText();
+                appWebChoice.getItems();
+                krajInput.getText();
+                //datePicker;
+                miastoInput.getText();
+                sqlTableView.getColumns().clear();
+                TableColumn first = new TableColumn("Test");
+                TableColumn second = new TableColumn("Test2");
+                sqlTableView.getColumns().addAll(first, second);
+            }
+        };
+        generateButton.setOnAction(buttonHandler);
 
         sqlTableView = new TableView();
-        grid.add(sqlTableView, 0,15, 60, 40);
+        grid.add(sqlTableView, 0, 15, 60, 40);
+        idInput.getText();
+        //sqlTableView.add(idInput);
+        sqlTableView.setEditable(true);
 
-        Text counter = new Text("Count: "+sqlCounter);
+        Text counter = new Text("Count: " + sqlCounter);
         grid.add(counter, 50, 55);
 
-        Scene scene = new Scene(grid, 1366,768);
+        Scene scene = new Scene(grid, 1366, 768);
         primaryStage.setScene(scene);
+
 
         primaryStage.show();
 
