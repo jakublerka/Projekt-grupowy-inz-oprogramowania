1b381331794dc9153209f2becef780527c9a0a76
Jakub Lerka <106154504+jakublerka@users.noreply.github.com>
2023-05-14 08:21:01 +0200

    Rebasing main branch here
    Due to a bad commit in master branch, rebasing main back here.

---
+++ Projekt-grupowy-inz-oprogramowania-master/src/main/java/com/example/maingui/mainGUIController.java
@@ -0,0 +1,17 @@
+package com.example.maingui;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+
+public class mainGUIController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ Projekt-grupowy-inz-oprogramowania-master/src/main/java/com/example/maingui/test.java
@@ -0,0 +1,4 @@
+package com.example.maingui;
+
+public class test {
+}
